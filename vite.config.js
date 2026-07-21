bfdb83c32b26af23b50067e8725e0b70288dcd3e
hallow_303 <adailtonsouza@acad.ifma.edu.br>
2026-07-21 14:10:14 -0300

    Simplify Vite config by removing command check

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
-export default defineConfig(({ command }) => ({
-  base: command === "serve" ? "/" : "/my-apod-site/",
-}));
+export default defineConfig({
+  base: '/my-apod-site/',
+})
